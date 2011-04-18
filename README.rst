ff6c9269e1005e35a3650d9f4e3527bb3b820ee7
Mark Lavin <markdlavin@gmail.com>
2011-04-18 16:22:58 -0400

    Initial backend.

--- README.rst
+++ README.rst
@@ -1,7 +1,23 @@
--*- restructuredtext -*-
-
 rmach
-=======
+==============
 
 Basic `Mach <http:/http://www.mach.com/>`_ backend for the `RapidSMS <http://www.rapidsms.org/>`_ project.
 
+Usage
+----------
+
+Add rmach to your Python path and setup the backend in your Django settings file. For example::
+
+    INSTALLED_BACKENDS = {
+        "mach": {
+            "ENGINE": "rmach.backend",
+            'host': 'localhost', 'port': '8888', # used for spawned backend WSGI server
+            'config': {
+                'id': 'XXXX',
+                'password': 'YYYYYYYY',
+                'number': '(###) ###-####',
+                'encoding': 'UTF-8', # optional message encoding
+                'encoding_errors': 'ignore', # optional encoding handling 
+            }
+        },
+    }
---
+++ rmach/backend.py
@@ -0,0 +1,94 @@
+import datetime
+import urllib
+import urllib2
+
+
+from django import http
+from django.core.exceptions import ImproperlyConfigured
+
+from rapidsms.backends.http import RapidHttpBackend
+
+
+class MachImproperlyConfigured(ImproperlyConfigured):
+    pass
+
+
+class MachBackend(RapidHttpBackend):
+
+    def configure(self, host="localhost", port=8080, config=None, **kwargs):
+        if "params_incoming" not in kwargs:
+            kwargs["params_incoming"] = "snr=%(phone_number)s&msg=%(message)s"
+        if "params_outgoing" not in kwargs:
+            kwargs["params_outgoing"] = "dnr=%(phone_number)s&msg=%(message)s"
+        if "gateway_url" not in kwargs:
+            kwargs["gateway_url"] = "http://gw1.promessaging.com/sms.php"
+        super(MachBackend, self).configure(host, port, **kwargs)
+        self.config = config
+        if 'id' not in self.config:
+            raise MachImproperlyConfigured(u"Mach backend missing account id")
+        if 'password' not in self.config:
+            raise MachImproperlyConfigured(u"Mach backend missing account password")
+        if 'number' not in self.config:
+            raise MachImproperlyConfigured(u"Mach backend missing account number")
+
+    def handle_request(self, request):
+        if request.method != 'POST':
+            self.info(u"Received request but wasn't POST. Doing nothing: %s" % request.GET)
+            return http.HttpResponseNotAllowed(['POST'])
+        self.info(u"Received request: %s" % request.POST)
+        msg = self.message(request.POST)
+        if msg:
+            self.route(msg)
+            return http.HttpResponse("OK")
+        else:
+            return http.HttpResponseBadRequest("")
+
+    def message(self, data):
+        sms = data.get(self.incoming_message_param, '')
+        sender = data.get(self.incoming_phone_number_param, '')
+        if not sms or not sender:
+            error_msg = u"ERROR: Missing %(msg)s or %(phone_number)s. parameters received are: %(params)s" % {
+                "msg" : self.incoming_message_param, 
+                "phone_number": self.incoming_phone_number_param,
+                "params": unicode(request.POST)
+            }
+            self.error(error_msg)
+            return None
+        now = datetime.utcnow()
+        try:
+            msg = super(MachBackend, self).message(sender, sms, now)
+        except Exception, e:
+            self.exception(e)
+            raise
+        return msg
+
+    def prepare_message(self, message):
+        encoding = self.config.get('encoding', 'ascii')
+        encoding_errors = self.config.get('encoding_errors', 'ignore')
+        data = {
+            "snr": self.config['number'],
+            "id": self.config['id'],
+            "pw": self.config['password'],
+            "dnr": message.connection.identity,
+            "msg": message.text.encode(encoding, encoding_errors),
+        }
+        return data
+
+    def send(self, message):
+        self.info(u"Sending message: %s" % message)
+        data = self.prepare_message(message)
+        self.debug(u"Sending data: %s" % data)
+        try:
+            response = urllib2.urlopen(self.gateway_url, urllib.urlencode(data))
+            for line in response:
+                if "-ERR" in line:
+                    # fail
+                    self.error(u"Error from gateway: %s" % line)
+                    self.debug(response)
+                    return False
+        except Exception, e:
+            self.exception(e)
+            return False
+        self.info('SENT')
+        self.debug(response)
+        return True
