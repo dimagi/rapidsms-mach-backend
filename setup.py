c27a5d341e012c29674f18f3b0d15bd325514f59
Mark Lavin <markdlavin@gmail.com>
2011-04-20 16:37:37 -0400

    Basic setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rmach',
+    version='0.1.0',
+    author='Dimagi',
+    packages=find_packages(),
+    include_package_data=True,
+    exclude_package_data={
+        '': ['*.sql', '*.pyc'],
+    },
+    url='https://github.com/dimagi/rapidsms-mach-backend',
+    description='RapidSMS Mach Backend',
+    long_description=open('README.rst').read(),
+)
